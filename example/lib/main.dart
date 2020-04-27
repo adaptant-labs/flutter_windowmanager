9880e1b93167615054bd585883dc9e322b9b57a6
Paul Mundt <paul.mundt@adaptant.io>
2020-04-27 11:27:50 +0200

    Update for AndroidX

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 void main() => runApp(MyApp());
